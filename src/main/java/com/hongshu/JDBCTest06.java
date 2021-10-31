7db56d2d0ee8a0f9c66d4dd4dd1fbfa325362d91
HongShu995 <759631477@qq.com>
2021-10-31 08:26:13 +0800

    test

--- src/main/java/com/hongshu/JDBCTest06.java
+++ src/main/java/com/hongshu/JDBCTest06.java
@@ -2,4 +2,14 @@ package com.hongshu;
 
 public class JDBCTest06
 {
+	public static void main(String[] args)
+	{
+		System.out.println("111111");
+		System.out.println("111111");
+		System.out.println("111111");
+		System.out.println("111111");
+		System.out.println("111111");
+		System.out.println("111111");
+		System.out.println("111111");
+	}
 }
